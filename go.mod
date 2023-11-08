3350f563d99e013d940aff593d04cc3aed43759a
Paulo <pauloperozocolombia@gmail.com>
2023-11-07 22:42:43 -0500

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/pauloperozo/jwtgo
+
+go 1.21.0
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func main() {
+
+	user := "Paulo"
+	email := "pauloperozo@gmail.com"
+	now := time.Now()
+	iat := now.Unix()
+	eat := now.AddDate(0, 0, 1).Unix()
+	secret := "this is secret"
+
+	payload := jwt.MapClaims{
+		"user":  user,
+		"email": email,
+		"iat":   iat,
+		"exp":   eat,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(tokenString)
+
+}
